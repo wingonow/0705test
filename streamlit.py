b8e7ff5bad748f630f66c9c082f836596231c4e4
wingonow <41103182+wingonow@users.noreply.github.com>
2022-07-05 17:05:32 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,5 +9,5 @@ doc = aw.Document(file)
 for paragraph in doc.get_child_nodes(aw.NodeType.PARAGRAPH, True) :
     paragraph = paragraph.as_paragraph()
 
-    print(paragraph.list_format + str(para))
+    print(paragraph.list_format + str(paragraph))
 
