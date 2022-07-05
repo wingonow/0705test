bb984afe007deee6a8d01fc0a37b3d4ed971a530
wingonow <41103182+wingonow@users.noreply.github.com>
2022-07-05 16:50:28 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,11 @@
 import aspose.words as aw
 import streamlit as st
 st.write('hello,world!')
+
+file = st.file_uploader('上传')
+
+doc = aw.Document(file)
+
+for para in doc.Paragraphs:
+    print(para.Range.ListFormat.ListString + str(para))
+
