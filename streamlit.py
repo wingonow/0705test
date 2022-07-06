b3aa7a47d5bcab9bbd62abbc97745d244a42a841
wingonow <41103182+wingonow@users.noreply.github.com>
2022-07-06 17:24:15 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -10,11 +10,31 @@ file = st.file_uploader('上传')
 
 doc=aw.Document(file)
 
+doc.update_list_labels()
+
+paras = [node.as_paragraph() for node in doc.get_child_nodes(aw.NodeType.PARAGRAPH, True)]
+# Find if we have the paragraph list. In our document, our list uses plain Arabic numbers,
+# which start at three and ends at six.
+for paragraph in paras:
+    if paragraph.list_format.is_list_item:
+        print(f"List item paragraph #{paras.index(paragraph)}")
+        # This is the text we get when getting when we output this node to text format.
+        # This text output will omit list labels. Strip any paragraph formatting characters.
+        paragraph_text = paragraph.to_string(aw.SaveFormat.TEXT).strip()
+        print(f"\tExported Text: {paragraph_text}")
+        label = paragraph.list_label
+        # This gets the position of the paragraph in the current level of the list. If we have a list with multiple levels,
+        # this will tell us what position it is on that level.
+        print(f"\tNumerical Id: {label.label_
+        # Combine them together to include the list label with the text in the output.
+        print(f"\tList label combined with text: {label.label_string} {paragraph_text}")
+
+
 #doc.save(r'C:\Users\liwenjing\Downloads\survey Q\new.html', SaveFormat.Html);
 
 #text = doc.range.text
 
-st.write(doc.to_string(aw.SaveFormat.TEXT))
+#st.write(doc.to_string(aw.SaveFormat.TEXT))
 
 #a=doc.save('output.html')
 
