22c9f3f448901e9fabcfb20010c30588ccb1797a
wingonow <41103182+wingonow@users.noreply.github.com>
2022-07-06 16:47:04 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -12,9 +12,9 @@ doc=aw.Document(file)
 
 #doc.save(r'C:\Users\liwenjing\Downloads\survey Q\new.html', SaveFormat.Html);
 
-#text = doc.range.text
+text = doc.range.text
 
-#st.write(text)
+st.write(text)
 
 #a=doc.save('output.html')
 
@@ -23,24 +23,23 @@ doc=aw.Document(file)
 #                           file_name='问卷脚本.html',
 #                           mime='html')
 
-plaintext = aw.PlainTextDocument(file)
+#plaintext = aw.PlainTextDocument(file)
 
-st.write(plaintext.text.strip())
+#st.write(plaintext.text.strip())
 
 
 
 #st.write(doc.to_string(aw.SaveFormat.TEXT))
-paras = doc.get_child_nodes(aw.NodeType.PARAGRAPH, True)
-st.write(type(paras))
-for para in paras:
-    para = para.as_paragraph()
-    st.write(doc.range.text)
-    if para.list_format.is_list_item:
-        st.write(f"This paragraph belongs to list ID# {para.list_format.list.list_id}, number style \"{para.list_format.list_level.number_style}\"")
-        st.write(f"\t\"{para.get_text().strip()}\"")
+#paras = doc.get_child_nodes(aw.NodeType.PARAGRAPH, True)
+#st.write(type(paras))
+#for para in paras:
+#    para = para.as_paragraph()
+#    st.write(doc.range.text)
+#    if para.list_format.is_list_item:
+#        st.write(f"This paragraph belongs to list ID# {para.list_format.list.list_id}, number style \"{para.list_format.list_level.number_style}\"")
+#        st.write(f"\t\"{para.get_text().strip()}\"")
 
-#for paragraph in doc.get_child_nodes(aw.NodeType.PARAGRAPH, True) :
-#    paragraph = paragraph.as_paragraph()
-#
-#    st.write(str(paragraph.list_format.list_level_number) + str(paragraph))
+for paragraph in doc.get_child_nodes(aw.NodeType.PARAGRAPH, True) :
+    paragraph = paragraph.as_paragraph()
+    st.write(str(paragraph.list_format.list_level_number) + str(paragraph))
 
