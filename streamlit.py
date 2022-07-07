9df16152404ff3a118e35ca97411a6ba3f71e04c
wingonow <41103182+wingonow@users.noreply.github.com>
2022-07-07 10:50:02 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -23,7 +23,8 @@ doc=aw.Document(file)
 paras = [node.as_paragraph() for node in doc.get_child_nodes(aw.NodeType.PARAGRAPH, True)]
 ## Find if we have the paragraph list. In our document, our list uses plain Arabic numbers,
 ## which start at three and ends at six.
-for paragraph in paras:
+for paragraph in paras:    
+    st.write('text',paragraph.to_string)
     st.write('labelstring',paragraph.list_label.label_string)
 #    if paragraph.list_format.is_list_item:
 #        print(f"List item paragraph #{paras.index(paragraph)}")
